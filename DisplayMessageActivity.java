43b24c0457b49ae1d83aa38cca11afd66c5217bb
JohnARC <ambioryarc@gmail.com>
2015-03-17 01:51:33 -0400

    temp message

--- DisplayMessageActivity.java
+++ DisplayMessageActivity.java
@@ -11,7 +11,18 @@ public class DisplayMessageActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);
+
+        // Create the text view
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+
+        // Set the text view as the activity layout
+        setContentView(textView);
     }
 
 
